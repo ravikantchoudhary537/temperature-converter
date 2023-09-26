4016317d85c0bf76a0fa88f7520d155ae9966237
Ravikant Choudhary <145194800+ravikantchoudhary537@users.noreply.github.com>
2023-09-26 21:53:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let cel = document.getElementById('cel');
+        let feh = document.getElementById('feh');
+        cel.addEventListener('input',function(){
+            let c = this.value;
+            let f = (c*9/5) + 32;
+            if(!Number.isInteger(f)){
+                f = f.toFixed(4);
+            }
+            feh.value = f;
+        });
+        feh.addEventListener('input',function(){
+           let f = this.value;
+           let c = (f - 32) * (5/9);
+            if(!Number.isInteger(c)){
+                c = c.toFixed(4);
+            }
+            cel.value = c;
+        });
